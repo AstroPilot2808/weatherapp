2e37c661e259b523b55d6fd4cb6775c3a7fae902
AstroPilot2808 <ajayohm28@gmail.com>
2023-03-31 22:08:35 -0400

    initial_setup_layout

---
+++ src/components/TopButtons.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+// Defining Array "Cities" in JavaScript with 5 terms
+function TopButtons() {
+    const cities = [
+        {
+            id:1,
+            title: 'London'
+        },
+        {
+            id:2,
+            title: 'Sydney'
+        },
+        {
+            id:3,
+            title: 'Tokyo'
+        },
+        {
+            id:4,
+            title: 'Toroto'
+        },
+        {
+            id:5,
+            title: 'Paris'
+        }
+    ]
+  return <div className="flex items-center justify-around my-6">
+    {/* Iterating each element of the array one my one */}
+    {cities.map((city) => (
+        <button key={city.id} className="text-white text-lg font-medium">{city.title}</button>
+    ))}
+  </div>
+}
+
+export default TopButtons
